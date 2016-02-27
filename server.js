19206f47ad4db35d3c38185d5db7cb1855bf376f
Alexei Scerbakov <scerbakov.alexei@gmail.com>
2016-02-27 13:08:57 +0100

    fixed bug

--- server.js
+++ server.js
@@ -25,20 +25,21 @@ app.get('/todos', function(req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
 
-        if (matchedTodo) {
-            res.json(matchedTodo);
-        } else {
-            res.status(404).send();
-        }
-    });
+	todos.forEach(function (todo) {
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
 
 app.listen(PORT, function() {
